e7b215c0af614c0970974e112607ff4f1056c849
Tanvi110 <73591941+Tanvi110@users.noreply.github.com>
2021-03-31 15:14:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+import AssetExample from './components/AssetExample';
+import AppHeader from './components/AppHeader';
+import Buttons from './components/Buttons';
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import ResultScreen from './screens/ResultScreen';
+import ThankYouScreen from './screens/ThankYouScreen';
+import Level1Screen from './screens/Level1Screen';
+import Level2Screen from './screens/Level2Screen';
+import Level3Screen from './screens/Level3Screen';
